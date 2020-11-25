0fa2301ef06ec822e81a37d29c09dea4e72e7219
RasssTor <4etkuu-poz@mail.ru>
2020-11-25 03:06:07 +0300

    Optimize, add comments

--- public/js/about.js
+++ public/js/about.js
@@ -1,11 +1,10 @@
-
+// define variables of vertical navigation line.
 const navLineHeight = $window.height()*0.7;
-let navLineElements = $('.about_nav li');
-let contentBlocks = $('.about_content .about_block');
+let navLineElements = $('.about_nav li'),
+    contentBlocks = $('.about_content .about_block');
 
-//ratio of content height to block size multiplied by height of navLine
-//used to correctly position nav elements on navLine
-
+// ratio of content height to block size multiplied by height of navLine.
+// used to correctly position nav elements on navLine.
 function createNavLine() {
     let contentHeight = $(document).height();
     for (let i = 0; i < 4; i++) {
@@ -16,6 +15,8 @@ function createNavLine() {
     $('.about_nav').slideDown(1000);
 }
 
+// change green line height according to user scrolling. Look like great scrollbar.
+// *height* - window height feature doesn't use to correctly working of navigation line.
 $window.on('scroll', updateNavLine);
 
 function updateNavLine() {
@@ -24,32 +25,39 @@ function updateNavLine() {
     navLineElements.eq(0).height(a/windowHeight*navLineHeight + 10)
 }
 
+// definition slider variables.
+// right border use for optimize. Don't fetching requests when user just click right arrow.
 const slider = $('.slider');
 let currPosition = 0;
 let rightBorder = -400;
 
-
+// async function for get random user information (first name, photo url)
 async function getUser() {
+    // create new slide with pseudo loading animation
     let newSlide = $('<div>').addClass('slide');
     slider.append(newSlide);
 
+    // await response from randomuser.me API
     let response = await fetch('https://randomuser.me/api/?inc=name,picture&gender=male').catch((err) => {
         console.log(err)
     });
+    // get body in json
     let user = await response.json().catch((err) => console.log(err));
 
     if (response.ok) {
         newSlide.append(
-            $('<img>').attr('src', user.results[0].picture.large),
+            $('<img alt="random man" src="">').attr('src', user.results[0].picture.large),
             $('<span>').text(user.results[0].name.first));
     } else {
         console.log("fetching error")
     }
 }
 
+// check out user's current position and right border's position to create (or not) new slide.
+// don't pay attention that promise from getUser ignored. // I add .then() to hide warning.
 function checkBorders() {
     if (currPosition <= rightBorder) {
-        getUser("next");
+        getUser("next").then();
         rightBorder -= 400;
     }
     if (currPosition === -400) {
@@ -58,6 +66,7 @@ function checkBorders() {
     }
 }
 
+// move slider. It seems like only one slide move.
 function moveSlider(direction) {
     checkBorders();
     if (direction) {
@@ -72,7 +81,9 @@ function moveSlider(direction) {
 }
 
 
-
+// ha-ha =)
+// It's joke too. (Last navigation element's name is "My credit card's data" and function below have you to scroll up
+// when you try to check it out
 function scrollJoke() {
     let top = $window.scrollTop();
     if (top > $(document).height() * 0.6) $window.scrollTop(0)
--- public/js/base.js
+++ public/js/base.js
@@ -1,27 +1,31 @@
-const navButtons = $('#main-navigation li:not(#return)');
-const returnButton = $('#return');
-const header = $('header');
-const footer = $('footer');
-const $window = $(window);
+//variables definition
+const navButtons = $('#main-navigation li:not(#return)'),
+      returnButton = $('#return'),
+      header = $('header'),
+      footer = $('footer'),
+      $window = $(window);
 let currentPage = "joke";
 
+//nav buttons click handler
 navButtons.click((event) => {
     navButtons.hide();
     setTimeout(() => returnButton.show(500), 500);
-    header.toggleClass("slow-move");                                        //on
+    header.toggleClass("slow-move");
     $window.off("scroll", onScroll);
     header.toggleClass("small");
     pageHandler(event);
 });
 
+//open navbar click handler
 returnButton.click(() => {
     header.toggleClass("small");
-    setTimeout(() => header.toggleClass("slow-move"), 1000); //on
+    setTimeout(() => header.toggleClass("slow-move"), 1000);
     $window.on("scroll", onScroll);
     returnButton.hide(500);
     setTimeout(() => navButtons.show(500), 600)
 });
 
+//auto hide/show navbar according to user scrolling
 let lastScrollTop = 0;
 
 function onScroll () {
@@ -37,27 +41,27 @@ function onScroll () {
 
 $window.on('scroll', onScroll);
 
-
+// "open" page with debug functions
 function pageHandler(event) {
     clearWindow();
     currentPage = event.target.id;
     if (currentPage === "about")  {
         $window.on('scroll', scrollJoke);
         $('.about_nav').slideUp();
-        console.log("yeah")
     } else {
         $window.off('scroll', scrollJoke);
     }
     if (currentPage === "contacts") {
         $('.contacts').css("display", "flex")
     } else {
-        $(document.getElementsByClassName(currentPage)).show(1000);
+        $('.' + currentPage).show(1000);
         if (currentPage === "about") {
             setTimeout(createNavLine, 2000)
         }
     }
 }
 
+// hide all "pages"
 function clearWindow() {
     setTimeout(() => $window.scrollTop(0), 500);
     $('main > section').hide(1000);
